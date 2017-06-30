78eea8e9ddeaf7d80381a1c2fb856bdf4b3226e6
Jay Letheby <jay.letheby@gmail.com>
2017-06-30 22:22:11 +1000

    navigation from main menu working

---
+++ JayCadSurveyXamarin/MenuPages/ConversionsMenuPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace JayCadSurveyXamarin.MenuPages
+{
+    public partial class ConversionsMenuPage : ContentPage
+    {
+        public ConversionsMenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- JayCadSurveyXamarin/MenuPages/MainMenuPage.xaml.cs
+++ JayCadSurveyXamarin/MenuPages/MainMenuPage.xaml.cs
@@ -7,9 +7,30 @@ namespace JayCadSurveyXamarin.MenuPages
 {
     public partial class MainMenuPage : ContentPage
     {
-        public MainMenuPage()
+		public MainMenuPage()
+		{
+			InitializeComponent();
+		}
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
         {
-            InitializeComponent();
+            Button selectedButton = (Button)sender;
+
+            switch (selectedButton.Text)
+            {
+                case "Conversions":
+                   await Navigation.PushAsync(new MenuPages.ConversionsMenuPage());
+                    break;
+				case "Angle Add/Subtract":
+					await Navigation.PushAsync(new ContentPages.AngleAddSubtractPage());
+					break;
+				case "About":
+					await Navigation.PushAsync(new ContentPages.AboutPage());
+					break;
+                default:
+                    break;
+            }
         }
+               
     }
 }
