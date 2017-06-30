c5dcb35c3bd47f3f1b27c711a0df7077289a70f1
Jay Letheby <jay.letheby@gmail.com>
2017-07-01 08:16:11 +1000

    all navigation working

--- JayCadSurveyXamarin/MenuPages/AngleConversionsMenuPage.xaml.cs
+++ JayCadSurveyXamarin/MenuPages/AngleConversionsMenuPage.xaml.cs
@@ -11,5 +11,25 @@ namespace JayCadSurveyXamarin.MenuPages
         {
             InitializeComponent();
         }
+
+		async void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			Button selectedButton = (Button)sender;
+
+			switch (selectedButton.Text)
+			{
+				case "Decimal Conversion":
+                    await Navigation.PushAsync(new ContentPages.DecimalAngleConversionPage());
+					break;
+				case "DEG/MIN/SEC Conversion":
+                    await Navigation.PushAsync(new ContentPages.DegreesMinutesSecondsConversionPage());
+					break;
+				case "Main Menu":
+					await Navigation.PopToRootAsync();
+					break;
+				default:
+					break;
+			}
+		}
     }
 }
--- JayCadSurveyXamarin/MenuPages/ConversionsMenuPage.xaml.cs
+++ JayCadSurveyXamarin/MenuPages/ConversionsMenuPage.xaml.cs
@@ -11,5 +11,28 @@ namespace JayCadSurveyXamarin.MenuPages
         {
             InitializeComponent();
         }
+
+		async void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			Button selectedButton = (Button)sender;
+
+			switch (selectedButton.Text)
+			{
+				case "Length Conversion":
+					await Navigation.PushAsync(new ContentPages.LengthConversionPage());
+					break;
+				case "Area Conversion":
+					await Navigation.PushAsync(new ContentPages.AreaConversionPage());
+					break;
+				case "Angle Conversions":
+                    await Navigation.PushAsync(new MenuPages.AngleConversionsMenuPage());
+					break;
+				case "Main Menu":
+                    await Navigation.PopToRootAsync();
+					break;
+				default:
+					break;
+			}
+		}
     }
 }
