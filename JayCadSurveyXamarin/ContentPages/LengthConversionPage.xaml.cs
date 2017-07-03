134d43d773a0c7df4150224246f60968677012e7
Jay Letheby <jay.letheby@gmail.com>
2017-07-03 18:42:24 +1000

    click events on buttons working

--- JayCadSurveyXamarin/ContentPages/LengthConversionPage.xaml.cs
+++ JayCadSurveyXamarin/ContentPages/LengthConversionPage.xaml.cs
@@ -12,21 +12,46 @@ namespace JayCadSurveyXamarin.ContentPages
             InitializeComponent();
         }
 
-		async void Handle_Clicked(object sender, System.EventArgs e)
+        async void Handle_Clicked(object sender, System.EventArgs e)
 		{
-			Button selectedButton = (Button)sender;
+            // Handle all click events here
+            // Remove spaces and convert to lowercase
 
-			switch (selectedButton.Text)
+			Button selectedButton = (Button)sender;
+            String buttonLabel = selectedButton.Text;
+            buttonLabel = buttonLabel.Replace(" ", "");
+            buttonLabel = buttonLabel.ToLower();
+         
+            switch (buttonLabel)
 			{
-				case "Main Menu":
+				case "main menu":
 					await Navigation.PopToRootAsync();
 					break;
-				case "Back":
+				case "back":
 					await Navigation.PopAsync();
+					break;
+                case "clear":
+                    await DisplayAlert("butt", "butt", "OK");
+                    break;
+				case "convert":
+					await DisplayAlert("pressed convert", "pressed convert", "OK");
+					break;
+				case "clearresult":
+					await DisplayAlert("pressed clear result", "clear result", "OK");
+					break;
+				case "clearstack":
+					await DisplayAlert("pressed clear stack", "clear stack", "OK");
+					break;
+				case "showstack":
+					await DisplayAlert("pressed show stack", "show stack", "OK");
 					break;
 				default:
 					break;
             }
 		}
+
+        private void Clear_ConvertFromUnits() {
+            
+        }
     }
 }
