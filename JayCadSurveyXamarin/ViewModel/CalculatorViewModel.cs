c8673839910930805cbd3e173bbacfc661301279
Jay Letheby <jay.letheby@gmail.com>
2018-03-25 14:59:07 +1000

    march 2018 update

--- JayCadSurveyXamarin/ViewModel/CalculatorViewModel.cs
+++ JayCadSurveyXamarin/ViewModel/CalculatorViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Windows.Input;
 using Xamarin.Forms;
+using System.Collections.Generic;
 
 namespace JayCadSurveyXamarin.ViewModel
 {
     public class CalculatorViewModel : BaseViewModel
     {
-        private string _outputResult;                       // Result displayed to screen. 
+        private string _outputResult;                           // Result displayed to screen. 
+        private List<string> _inputs = new List<string>();      // List to hold user calculator inputs
+        private List<string> _operators = new List<string>();   // Operators list for validation without the .
+        private List<int> _numbers = new List<int>();           // Numbers for validation
+        private string _currentOutput;
 
 		public string OutputResult
 		{
@@ -25,16 +30,37 @@ namespace JayCadSurveyXamarin.ViewModel
 			OperandCommand = new Command<string> (Operand);                                    
 			ScientificOperatorCommand = new Command<string> (ScientificOperator);
 
-            _outputResult = "0";
+            _outputResult = "ass";
+            _currentOutput = "0";
+
+            // initialise validation lists
+            InitialiseLists();
 		}
+
+        private void InitialiseLists()
+        {
+            int num = 0;
+
+            // Fill numbers list
+            while (num < 10)
+            {
+                _numbers.Add(num);
+                num++;
+            }
+
+            // Fill Operators List
+            _operators.Add("+");
+            _operators.Add("-");
+            _operators.Add("x");
+            _operators.Add("÷");
+        }
 
         private void Operator(string value) 
         {
-            _outputResult = "";
+           _outputResult = ""; 
+           OnPropertyChanged(OutputResult);
 
-            OnPropertyChanged(OutputResult);
-
-			_outputResult = value;
+            _outputResult = value;
 
 			OnPropertyChanged(OutputResult);
 
@@ -57,7 +83,7 @@ namespace JayCadSurveyXamarin.ViewModel
 
 			OnPropertyChanged(OutputResult);
 
-            _outputResult = value;
+            _outputResult =  value;
 
 			OnPropertyChanged(OutputResult);
 		}
